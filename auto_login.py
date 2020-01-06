55102d0bf4a1c054f3419f47bbfbdfe8951a44c1
jhonatanmsc <jhonmscosta@gmail.com>
2020-01-06 14:46:14 -0300

    ADDED: auto login script

---
+++ auto_login.py
@@ -0,0 +1,47 @@
+'''
+    Script de auto login na rede do ifpi campus central
+    ===================================================
+
+    Author: Jhonatanmsc
+    Github: https://github.com/jhonatanmsc/auto-login
+'''
+
+import pdb
+
+def load_env():
+    env_vars = {}
+    with open('.env') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            # if 'export' not in line:
+            #     continue
+            # Remove leading `export `, if you have those
+            # then, split name / value pair
+            # key, value = line.replace('export ', '', 1).strip().split('=', 1)
+            key, value = line.strip().split('=', 1)
+            # os.environ[key] = value  # Load to local environ
+            env_vars[key.replace(' ', '')] = value.replace(' ', '')
+    
+    return env_vars
+
+def internet_is_ok():
+    pass
+
+def login():
+    pass
+
+def main():
+    env = load_env()
+    
+    while(not internet_is_ok()):
+        print('Applying login...')
+        login()
+    else:
+        print("Internet is ok.")
+
+    print(env)
+
+
+if __name__ == '__main__':
+    main()
